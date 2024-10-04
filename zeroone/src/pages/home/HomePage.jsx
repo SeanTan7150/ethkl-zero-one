1ca4b9b26157f803d6d2f00cfc8ca24c628301e2
jianwei <jianwei2102@gmail.com>
2024-10-05 07:46:48 +0800

    feat: retrieve all artist for discover

--- zeroone/src/pages/home/HomePage.jsx
+++ zeroone/src/pages/home/HomePage.jsx
@@ -1,37 +1,25 @@
-const createUser = async () => {
-  // Dummy data for user creation
-  const dummyData = {
-    address: "0x69e75a2346ae86c1c70f91216e464811a99ed87f",
-    // username: "Justin Bieber",
-    // profile_pic_url:
-    //   "https://variety.com/wp-content/uploads/2017/09/justin_bieber.png", // Example profile picture URL
-    // bio: `Justin Bieber is a Grammy-winning Canadian singer-songwriter known for hits like "Baby" and "Sorry." Rising to fame as a teen, he connects with millions through his evolving music and philanthropic efforts.`,
-  };
-
-  try {
-    const res = await fetch("http://localhost:5001/api/user/createUser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dummyData), // Include the dummy data in the request body
-    });
-
-    if (!res.ok) {
-      throw new Error("Verification failed."); // Handle failed verification
-    }
-
-    const result = await res.json(); // Parse the response to JSON
-    console.log("User created successfully:", result); // Log the success message and result
-  } catch (error) {
-    console.error("Error creating user:", error.message); // Log any errors encountered
-  }
-};
+import React from "react";
 
 export default function HomePage() {
-  return (
-    <div>
-      <button onClick={createUser}>Create User</button>
-    </div>
-  );
+  const [allArtists, setAllArtists] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchArtists = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/api/user/getArtists");
+        if (!res.ok) {
+          throw new Error("Failed to fetch artists");
+        }
+        const data = await res.json();
+        setAllArtists(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchArtists();
+  }, []);
+
+  return <div>{allArtists.length !== 0 ? "Artist" : "No"}</div>;
 }
