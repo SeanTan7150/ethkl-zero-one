a2e86a541fce4ec951868dc7717b6dd30b44ae21
rand0mguylol <72346097+rand0mguylol@users.noreply.github.com>
2024-10-05 08:36:28 +0800

    style: Home Page

--- zeroone/src/pages/home/HomePage.jsx
+++ zeroone/src/pages/home/HomePage.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Tabs,
+  Tab,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+} from "@mui/material";
+
 export default function HomePage() {
   const [allArtists, setAllArtists] = React.useState([]);
 
@@ -21,5 +39,54 @@ export default function HomePage() {
     fetchArtists();
   }, []);
 
-  return <div>{allArtists.length !== 0 ? "Artist" : "No"}</div>;
+  // return <div>{allArtists.length !== 0 ? "Artist" : "No"}</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          flex: 1,
+          p: 2,
+          maxWidth: 1280,
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Box sx={{ p: 3, mb: 3, backgroundColor: "#fff", borderRadius: "8px" }}>
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: "bold",
+              textAlign: "start",
+            }}
+          >
+            GM
+          </Typography>
+          <Box>
+            <Card sx={{ display: "flex", maxWidth: "300px", height: "80px" }}>
+              <CardMedia
+                component="img"
+                sx={{ width: 80 }}
+                image="src/assets/store/store-placeholder.jpg"
+                alt="Live from space album cover"
+              />
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h5">
+                    Live From Space
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    component="div"
+                    sx={{ color: "text.secondary" }}
+                  >
+                    Mac Miller
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
